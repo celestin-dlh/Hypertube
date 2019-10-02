537d5faa7c3cba30e45241a9c3dfe02ea14ccb8e
Celestin DELAHAYE <cdelahay@e1r12p14.42.fr>
2019-10-02 11:15:29 +0200

    Header and Footer added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+
+/* Layout */
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+        <Router>
+
+
+        </Router>
+      <Footer />
     </div>
   );
 }
