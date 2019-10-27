684ec0116b3c4e331207b7494203b41b0fb9babd
unknown <celestdelahaye@gmail.com>
2019-10-27 21:59:33 +0100

    save temp menu sidebar

--- front-end/src/components/userProfile/Profile.js
+++ front-end/src/components/userProfile/Profile.js
@@ -1,32 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 /* Bootstrap */
 import Container    from 'react-bootstrap/Container';
-import Row          from 'react-bootstrap/Row';
-import Col          from 'react-bootstrap/Col';
 import Navbar       from 'react-bootstrap/Navbar';
-import Nav          from 'react-bootstrap/Nav';
-import NavDropdown  from 'react-bootstrap/NavDropdown';
 import Form         from 'react-bootstrap/Form';
 import FormControl  from 'react-bootstrap/FormControl';
 import Button       from 'react-bootstrap/Button';
 
 
 /* Style */
-import '../style/profile.css';
+import '../style/menu.css';
 
 
-export default function Profil() {
+export default function Profile() {
+    const [enableMenu, setEnableMenu] = useState('menu menu-not-toggle');
+
+    const handleClick = () => {
+        if (enableMenu === "menu menu-not-toggle") {
+            setEnableMenu('menu menu-toggle')
+        } 
+        else {
+            setEnableMenu('menu menu-not-toggle')
+        }
+    }
 
     return (
         <Container fluid={true} style={{paddingLeft: "0px", paddingRight: "0px"}}> 
-            <Navbar bg="light" sticky="top">
-                <Navbar.Brand href="/home">Hypertube</Navbar.Brand>
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="light">Search</Button>
-                    </Form>
-                <img width="32" src="./images/default_avatar.png" />
+            <Navbar sticky="top" style={{backgroundColor: "grey", display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+                <div>
+                    <img onClick={handleClick} width="32" src="./images/toggle-button.jpg"/>
+                        
+                    <div id="menu" className={enableMenu}>
+                        
+                        <a href="/">
+                            <div className="menuCell">
+                                <div className="content">
+                                    <img width="24" src="./images/home.png" />
+                                    <p>Accueil</p>                                  
+                                </div>
+                            </div>
+                        </a>
+
+                        <a href="/settings">
+                            <div className="menuCell">
+                                <div className="content">
+                                    <img width="24" src="./images/settings.png"/>
+                                    <p>Parametres</p>                                  
+                                </div>
+                            </div>
+                        </a>
+
+                        <a href="/logout">
+                            <div className="menuCell">
+                                <div className="content">
+                                    <img width="24" src="./images/logout.png"/>
+                                    <p>Logout</p>                                  
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                    
+                    <Navbar.Brand href="/home">Hypertube</Navbar.Brand>        
+                </div>
+
+                <Form inline>
+                    <FormControl type="text" placeholder="Search a movie..." className="mr-sm-2" />
+                    <Button variant="light">Search</Button>
+                </Form>
+                <div>
+                    <img style={{textAlign: "right"}} width="32" src="./images/default_avatar.png" />
+
+                </div>
+
             </Navbar>
         </Container>
     )
