7e9411a25669e5ea3effa23630c2a8812770963b
frrobert <frrobert@student.42.fr>
2019-11-05 14:19:05 +0100

    no front

--- front-end/src/components/auth/Jwt.js
+++
@@ -1,7 +0,0 @@
-//import React, { usestate, useEffect } from 'react';
-
-export default function Jwt(props) {
-	let token = props.match.params.token;
-	localStorage.setItem('token', token);
-	window.location.replace("http://localhost:3000/");
-}
