b241b09f730de79df3f6c1b40449995d38129877
Célestin Delahaye <45206055+Zeeeratul@users.noreply.github.com>
2019-11-05 16:18:12 +0100

    settings

--- front-end/src/components/userProfile/Settings.js
+++ front-end/src/components/userProfile/Settings.js
@@ -9,18 +9,73 @@ import Form from 'react-bootstrap/Form';
 
 /* Style */
 import '../style/Settings.css';
+import '../style/Input.css';
 
 /* Templates */ 
 import Header from '../templates/Header';
 
 function UpdatePassword() {
+
+
 	return (
-		<div>
-			<h1>Update Password</h1>
-			<form>
-				<input type="text" placeholder="Password"/>
+		<div className="menu-settings" style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+			<h3>Update your password</h3>
+			<form className="settings-form">
+				<div className="input-form">
+					<label htmlFor="inp" className="inp">
+						<input className="input" type="password" placeholder="&nbsp;" name="password" value="test"/>
+						<span className="label">Password</span>
+						<span className="border"></span>
+					</label>
+				</div>
+				<div className="input-form">
+					<label htmlFor="inp" className="inp">
+						<input className="input" type="password" placeholder="&nbsp;" name="password" value="test"/>
+						<span className="label">New password</span>
+						<span className="border"></span>
+					</label>
+				</div>
+				<div className="input-form">
+					<label htmlFor="inp" className="inp">
+						<input className="input" type="password" placeholder="&nbsp;" name="password" value="test"/>
+						<span className="label">Confirm Password</span>
+						<span className="border"></span>
+					</label>
+				</div>
 			</form>
 		</div>
+	)
+}
+
+function UpdateInfos() {
+	return (
+		<div className="menu-settings" style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+			<h3>Update your Informations</h3>
+			<form className="settings-form">
+				<div className="input-form">
+					<label htmlFor="inp" className="inp">
+						<input className="input" type="text" placeholder="&nbsp;" name="password" value="test"/>
+						<span className="label">First Name</span>
+						<span className="border"></span>
+					</label>
+				</div>
+				<div className="input-form">
+					<label htmlFor="inp" className="inp">
+						<input className="input" type="text" placeholder="&nbsp;" name="password" value="test"/>
+						<span className="label">Last Name</span>
+						<span className="border"></span>
+					</label>
+				</div>
+				<div className="input-form">
+					<label htmlFor="inp" className="inp">
+						<input className="input" type="email" placeholder="&nbsp;" name="password" value="test"/>
+						<span className="label">Email</span>
+						<span className="border"></span>
+					</label>
+				</div>
+			</form>
+		</div>
+
 	)
 }
 
@@ -31,9 +86,15 @@ function Menu(props) {
 		)
 	}
 
+	else if (props.component === "infos") {
+		return (
+			<UpdateInfos />
+		)
+	}
+
 	else {
 		return (
-			<h1>Infos</h1>
+			<h1>test</h1>
 		)
 	}
 }
@@ -43,8 +104,8 @@ function Settings() {
 	const [component, setComponent] = useState('infos');
 
 	const handleClick = (event) => {
-		setComponent(event.target.id)
-		console.log(event.target.id);
+		setComponent(event.target.value)
+		console.log(event.target.value);
 	}
 
 
@@ -52,15 +113,15 @@ function Settings() {
 		<Container fluid style={{padding: "0px"}}>
 			<Header/>
 			<Row className="justify-content-center" style={{height: "95vh"}}>
-				<Col md="2" className="">
-
+				<Col md="2" className="" style={{margin: "auto"}}>
 					<div className="menu-settings" style={{display: "flex", flexDirection: "column", backgroundColor: "#272727"}}>
-						<div onClick={handleClick} id="infos" className="cell">
-							<p>Update Infos</p>
-						</div>
-						<div onClick={handleClick} id="password" className="cell">
-							<p>Update Password</p>
-						</div>
+						<h3>Celestin Delahaye</h3>
+							<button onClick={handleClick} value="infos">
+								<p>Update Infos</p>
+							</button>
+							<button onClick={handleClick} value="password">
+								<p>Update Password</p>
+							</button>
 						<div className="cell">
 							<p>Change background</p>
 						</div>
@@ -69,7 +130,7 @@ function Settings() {
 
 
 				</Col>
-				<Col md="8" className="">
+				<Col md="6" className="" style={{margin: "auto"}}>
 					<Menu component={component}/>
 
 				</Col>
