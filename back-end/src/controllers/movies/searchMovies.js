f43dc541972be9c6cab1582f7c0e49a9e5f340cb
frrobert <frrobert@student.42.fr>
2019-11-19 10:22:55 +0100

    push before merge

--- back-end/src/controllers/movies/searchMovies.js
+++ back-end/src/controllers/movies/searchMovies.js
@@ -1,23 +1,49 @@
 const axios = require('axios');
 const {isNumber} = require("@hapi/joi/lib/common");
 
-const searchmovie = async function(req, result) {
-    let movie, page, lang, sort;
+function addPage(movie, page) {
+    page = Number(page);
+    if (page < movie.total_pages)
+    {
+        console.log(movie.total_pages);
+    }
+    else {
+        console.log("else " + movie.total_pages);
+    }
+    return movie
+}
 
-    await isNaN(req.params.lang) ? lang = req.params.lang :  page = req.params.lang;
-    await isNumber(req.params.lang) ? lang = 'en-EN' : page = req.params.lang;
-    await req.params.lang === undefined ? lang = 'en-EN' :  lang = req.params.lang;
+const searchmovie = async function(req, result) {
+    let movie, page, lang;
+
+    req.params.lang === 'undefined' ? lang = "en-EN" :  lang = req.params.lang;
+    req.params.page === 'undefined' ? page = 1 :  page = req.params.page;
+
 
     axios.get('https://api.themoviedb.org/3/search/movie/?api_key=' + process.env.MOVIEDB_API_KEY +
-           '&query=' + req.params.query + '&include_adult=true&language=' + lang +'&page=' + page)
+        '&query=' + req.params.query + '&include_adult=true&language=' + lang +'&page=' + page)
         .then( response => {
                 console.log("Search Movie with query : "+ req.params.query +
-                            " Language : "+ lang + " Page : " + page);
+                    " Language : "+ lang + " Page : " + page);
                 movie = response.data;
             }
         ).then(() => {
-            result.json(movie)
-        })
+
+            axios.get('https://api.themoviedb.org/3/search/movie/?api_key=' + process.env.MOVIEDB_API_KEY +
+                '&query=' + req.params.query + '&include_adult=true&language=' + lang +'&page=' + (Number(page)+1))
+                .then( response => {
+                        console.log("Search Movie with query : "+ req.params.query +
+                            " Language : "+ lang + " Page : " + (Number(page)+1));
+                        for (let i=0; i<20; i++) {
+                            let numbeResult = Number(page)*20;
+                            movie['results'][numbeResult+i] = response.data.results[i];
+                        }
+                    }
+                ).then(() => {
+                result.json(movie)
+            })
+        }
+    )
         .catch(error => {
                 console.log(error);
             }
